6946a11def5356c91796dbb57d94d94e336f3481
AsakaJX <yourasaka@protonmail.com>
2025-09-04 18:58:22 +0300

    feat: completed logic, styling of form

--- frontend/main.js
+++ frontend/main.js
@@ -7,29 +7,68 @@ import {
 
 console.log('adding test books to library...');
 
-addBookToLibrary('Harry Potter', 'idk', true);
-addBookToLibrary('Harry Potter 2', 'idk 2', true);
-addBookToLibrary('Harry Potter 3', 'idk 3', false);
-
-printAllBooks();
+addBookToLibrary('Test 1', 'test', true);
+addBookToLibrary('Test 2', 'test', true);
+addBookToLibrary('Test 3', 'test', true);
+addBookToLibrary('Test 4', 'test', true);
 
 const booksList = document.querySelector('#book-list');
 
-library.forEach((elem) => {
-  const bookChild = document.createElement('li');
+function redrawListElements() {
+  const allElements = document.querySelectorAll('#book-list li');
+  allElements.forEach((item) => {
+    booksList.removeChild(item);
+  });
 
-  bookChild.setAttribute('id', elem.id);
+  library.forEach((elem) => {
+    const bookChild = document.createElement('li');
 
-  // adding book info elements
-  bookChild.innerHTML = `<div id="book-main-container"><p class="book-title-label">Title</p><p class="book-author-label">Author</p><p class="book-status-label">Status</p><p class='book-title'>${elem.title}</p><p class='book-author'>${elem.author}</p><p class='book-status'>${elem.isRead ? 'Already read' : "Hasn't been read yet"}</p></div>`;
+    bookChild.setAttribute('id', elem.id);
 
-  // adding book management button elements
-  bookChild.innerHTML += `<div class="book-buttons-container"><button class="book-read-button">${elem.isRead ? 'Not read' : 'Read'}</button><button class="book-remove-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg></button></div>`;
+    // adding book info elements
+    bookChild.innerHTML = `<div id="book-main-container"><p class="book-title-label">Title</p><p class="book-author-label">Author</p><p class="book-status-label">Status</p><p class='book-title'>${elem.title}</p><p class='book-author'>${elem.author}</p><p class='book-status'>${elem.isRead ? 'Already read' : "Hasn't been read yet"}</p></div>`;
 
-  booksList.appendChild(bookChild);
+    // adding book management button elements
+    bookChild.innerHTML += `<div class="book-buttons-container"><button class="book-read-button">${elem.isRead ? 'Not read' : 'Read'}</button><button class="book-remove-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg></button></div>`;
+
+    booksList.appendChild(bookChild);
+  });
+}
+
+redrawListElements();
+
+// add book dialog
+
+const dialog = document.querySelector('#add-book-dialog');
+const openDialogButton = document.querySelector('#add-book-btn');
+const closeDialogButton = document.querySelector('#close-add-book-dialog');
+
+const bookTitle = document.querySelector('#add-book-title');
+const bookAuthor = document.querySelector('#add-book-author');
+const bookStatus = document.querySelector('#add-book-status');
+
+openDialogButton.addEventListener('click', (event) => {
+  bookTitle.value = '';
+  bookAuthor.value = '';
+  bookStatus.checked = false;
+  dialog.showModal();
 });
 
-// Book management logic
+closeDialogButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (bookTitle.value.length <= 0 || bookAuthor.value.length <= 0) {
+    console.log('empty');
+    return;
+  }
+
+  addBookToLibrary(bookTitle.value, bookAuthor.value, bookStatus.checked);
+  redrawListElements();
+
+  dialog.close();
+});
+
+// book management logic
 
 const bookRemoveBtn = document.querySelectorAll(
   '#book-list li .book-buttons-container .book-remove-button'
